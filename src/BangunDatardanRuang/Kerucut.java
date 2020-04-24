214a6fe1ccc2e4f915661cba8c76563939a78317
iffatuzzahra <54696540+iffatuzzahra@users.noreply.github.com>
2020-04-24 13:54:31 +0700

    Update Kerucut.java

--- src/BangunDatardanRuang/Kerucut.java
+++ src/BangunDatardanRuang/Kerucut.java
@@ -1,17 +1,18 @@
 package BangunDatardanRuang;
+import java.math.*;
 
-public class Kerucut extends Lingkaran{
+public class Kerucut extends Lingkaran implements BangunRuang{
     private float tinggi;
    
     public Kerucut(float jari, float tinggi) {
         super(jari);
         this.tinggi = tinggi;
     }
-    public double getVolumeKerucut() {
+    public double getVolume() {
         double volumeKerucut = (super.Luas() * tinggi )/3;
         return volumeKerucut;
     }
-    public double getKelilingKerucut() {
+    public double getLuasPermukaan() {
         double garisPelukis = Math.sqrt(Math.pow(super.getJari(),2)+Math.pow(tinggi,2));
         double permukaanKerucut = super.Luas()+Math.PI*super.getJari()*garisPelukis;
         return permukaanKerucut;
