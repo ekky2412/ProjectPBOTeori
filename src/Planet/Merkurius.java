046101f47c4c74c1b05e1e5dcd78f4ea0575ea45
Ayola Ika Arihta <64198850+ayolaikaa@users.noreply.github.com>
2020-05-11 18:45:55 +0700

    Add files via upload

---
+++ src/Planet/Merkurius.java
@@ -0,0 +1,15 @@
+package Planet;
+
+public class Merkurius extends Planet {
+    
+    double jari = 2439.7;
+    double kalarotasi = 59;
+    double kalarevolusi = 88;
+    
+    public Merkurius(int jari, double orbit, double kalarotasi, double kalarevolusi) {
+        super(jari, orbit, kalarotasi, kalarevolusi);
+        
+        
+    }
+    
+}
